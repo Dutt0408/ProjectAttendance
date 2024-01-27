ec685dd429021153ca6ab4bcddea1ed86def9857
Dutt Patel <127007807+Dutt0408@users.noreply.github.com>
2024-01-27 16:00:38 -0500

    Commit Mahant

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 
 // import './App.css';
 // import './index.css'
-// import ReactFinalFormDemo from './ReactFinalFormDemo'
+
 import Navbar from './navbar'
-import { ReactFinalFormDemo } from "./ReactFinalFormDemo";
+
+
 
 function App() {
   return (
    <>
-    <ReactFinalFormDemo/>
+
     <Navbar/>
     
     
